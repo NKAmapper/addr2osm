182a7d280431146bf93168314f948166d7350e08
NKA <nkamapper@gmail.com>
2018-11-28 14:53:39 +0100

    New version with "-manual" mode and recurse up
    Additions in this version:
    * Recurse up: Checks if node is member of way or relation; creates new address node
    * Creates new address node if no "pure" address node already exists, even if address is already part of way of relation (e.g. buildings)
    * New "-manual" mode: Includes copy of surplus addresses (nodes with also other tags, ways and relations) + includes DELETE tag instead of deleting node for easier verification

--- addr2osm.py
+++ addr2osm.py
@@ -3,8 +3,9 @@
 
 # ADDR2OSM.PY
 # Loads addresses from Kartverket and creates an osm file with updates
-# Usage: python addr2osm.py <kommunenummer>
+# Usage: "python addr2osm.py <kommunenummer> [-manual]""
 # Creates "new_addresses_xxxx_xxxxxx.osm" file
+# Optional "-manual" parameter will add DELETE tag instead of deleting node + include surplus addr objects
 
 
 import json
@@ -20,7 +21,7 @@ from xml.etree import ElementTree
 from itertools import tee
 
 
-version = "0.1.0"
+version = "0.2.0"
 
 
 escape_characters = {
@@ -84,25 +85,36 @@ def find_element (id_no):
 
 
 # Output osm object to file with all tags and children/members from Overpass
-# Parameter "action" is "delete" or empty 
+# Parameter "action" is "delete", "modify", "new" or "output".
 
-def osm_element (element, delete):
+def osm_element (element, action):
 
-	if delete:
-		action = "action='delete' "
-	else:
+	global osm_id
+	global debug
+
+	if action == "delete":
+		if debug:
+			action = ""
+			element['tags']['DELETE'] = "yes"
+		else:
+			action = "action='delete' "
+	elif action == "modify":
+		action = "action='modify' "
+	elif action == "output":
 		action = ""
 
-	header = u"  <%s id='%i' %stimestamp='%s' uid='%i' user='%s' visible='true' version='%i' changeset='%i'"\
+	if action == "new":
+		osm_id -= 1
+		osm_line ("  <node id='%i' action='modify' visible='true' lat='%f' lon='%f'>\n" % (osm_id, element['lat'], element['lon']))
+	else:
+		header = u"  <%s id='%i' %stimestamp='%s' uid='%i' user='%s' visible='true' version='%i' changeset='%i'"\
 				% (element['type'], element['id'], action, element['timestamp'], element['uid'], element['user'],\
 				element['version'], element['changeset'])
-
-	if element['type'] == "node":
-		header = header + " lat='%f' lon='%f'>\n" % (element['lat'], element['lon'])
-	else:
-		header = header + ">\n"
-
-	osm_line (header)
+		if element['type'] == "node":
+			header = header + " lat='%f' lon='%f'>\n" % (element['lat'], element['lon'])
+		else:
+			header = header + ">\n"
+		osm_line (header)
 
 	if "nodes" in element:
 		for node in element['nodes']:
@@ -120,13 +132,14 @@ def osm_element (element, delete):
 
 	# Recursively output child/member objects if any
 
-	if "nodes" in element:
-		for node in element['nodes']:
-			osm_element (find_element(node), delete=False)
+	if not(action):
+		if "nodes" in element:
+			for node in element['nodes']:
+				osm_element (find_element(node), action="outut")
 
-	if "members" in element:
-		for member in element['members']:
-			osm_element (find_element(member['ref']), delete=False)
+		if "members" in element:
+			for member in element['members']:
+				osm_element (find_element(member['ref']), action="output")
 
 
 # Main program
@@ -138,8 +151,13 @@ if __name__ == '__main__':
 
 	if (len(sys.argv) == 2) and (len(sys.argv[1]) == 4) and sys.argv[1].isdigit():
 		municipality_id = sys.argv[1]
+		debug = False
+	elif (len(sys.argv) == 3) and (len(sys.argv[1]) == 4) and sys.argv[1].isdigit() and (sys.argv[2] == "-manual"):
+		municipality_id = sys.argv[1]
+		debug = True
 	else:
-		sys.exit ('Usage: Please type "python addr2osm.py <nnnn>" with 4 digit municipality number')
+		sys.exit ('Usage: Please type "python addr2osm.py <nnnn>" with 4 digit municipality number\n'\
+					+ '       Add "-manual" to get surplus address objects and DELETE tag\n')
 
 	# Load municipality id's and names from Kartverket code list
 
@@ -186,34 +204,67 @@ if __name__ == '__main__':
 	# Load existing addr nodes in OSM for municipality, then recurse down to get any children/members
 
 	message ("Loading existing addresses for %s %s from OSM... " % (municipality_id, municipality[municipality_id]))
-	query = '[out:json][timeout:60];(area[ref=%s][admin_level=7];)->.a;(nwr["addr:street"](area.a););out center meta;' % (municipality_id)
+	query = '[out:json][timeout:60];(area[ref=%s][admin_level=7];)->.a;(node["addr:street"](area.a););out center meta;' % (municipality_id)
+	if debug:
+		query = query.replace("node", "nwr")
+
 	file = urllib2.urlopen("https://overpass-api.de/api/interpreter?data=" + urllib.quote(query))
 	osm_data = json.load(file)
 	file.close()
-	message ("%i " % len(osm_data['elements']))
+	message ("%i" % len(osm_data['elements']))
 
-	query = query.replace("out center meta", ">;out meta")
+	# Recurse up to get any parents
+
+	query = query.replace("out center meta", "<;out meta")
 	file = urllib2.urlopen("https://overpass-api.de/api/interpreter?data=" + urllib.quote(query))
-	osm_children = json.load(file)
+	osm_parents = json.load(file)
 	file.close()
-	message ("(+ %i child objects)\n" % len(osm_children['elements']))
+
+	parents = set()  # Will contain the id for children elements
+
+	for element in osm_parents['elements']:
+		if "nodes" in element:
+			for node in element['nodes']:
+				parents.add(node)
+
+		if "members" in element:
+			for member in element['members']:
+				parents.add(member['ref'])
+
+	message (" +%i parent objects" % len(osm_parents['elements']))
+	osm_parents = None  # No longer needed
+
+	if debug:
+		query = query.replace("<;out meta", ">;out meta")
+		file = urllib2.urlopen("https://overpass-api.de/api/interpreter?data=" + urllib.quote(query))
+		osm_children = json.load(file)
+		file.close()
+		message (" +%i child objects" % len(osm_children['elements']))
+	else:
+		osm_children = { 'elements': [] }
+
+	message("\n")
 
 	# Load latest address file for municipality from Kartverket
 
 	filename = "Basisdata_%s_%s_4258_MatrikkelenVegadresse_CSV" % (municipality_id, municipality_name)
-	message ("Loading address file %s from Kartverket\n" %filename)
+	message ("\nLoading address file %s from Kartverket\n" %filename)
 	file_in = urllib2.urlopen("https://nedlasting.geonorge.no/geonorge/Basisdata/MatrikkelenVegadresse/CSV/" + filename + ".zip")
 	zip_file = zipfile.ZipFile(StringIO.StringIO(file_in.read()))
 	csv_file = zip_file.open(filename + "/matrikkelenVegadresse.csv")
 	addr_table1, addr_table2 = tee(csv.DictReader(csv_file, delimiter=";"), 2)
 
+	# Open outut file
+
+	filename = "Address_import_%s_%s.osm" % (municipality_id, municipality_name)
+	file_out = open(filename, "w")
+	osm_line ("<?xml version='1.0' encoding='UTF-8'?>\n")
+	osm_line ("<osm version='0.6' generator='addr2osm v%s' upload='false'>\n" % version)
 
 	# Initiate loop
 
 	osm_id = -1000
-	debug = False
 	corrected = 0
-	corrected_new = 0
 	added = 0
 	modified = 0
 	deleted = 0
@@ -252,21 +303,42 @@ if __name__ == '__main__':
 			street = street.replace("'", u"’")
 			found_index = -1
 
-			# Loop existing addr objects from OSM to find exact matches (also for multiple matches)
-			# Consider a match if distance is less than 1/1000 degrees
+			# Loop existing addr objects from OSM to find first exact match of "pure" address node
 
 			for osm_object in osm_data['elements']:
 				found_index += 1
-				if "center" in osm_object:
-					distance = math.sqrt((osm_object['center']['lat'] - latitude) ** 2 + (osm_object['center']['lon'] - longitude) ** 2)
-				else:
-					distance = math.sqrt((osm_object['lat'] - latitude) ** 2 + (osm_object['lon'] - longitude) ** 2)
-				if distance < 0.001:
-					tag = osm_object['tags']
-					if ('addr:housenumber' in tag) and ('addr:postcode' in tag) and ('addr:city' in tag):
-						if (housenumber == tag['addr:housenumber']) and (street == tag['addr:street']) and (postcode == tag['addr:postcode']) and (city == tag['addr:city']):
-							found[checked] = True
-							del osm_data['elements'][found_index]  # Remove match to speed up looping
+
+				tag = osm_object['tags']
+				if (osm_object['type'] == "node") and (len(tag) == 4) and ('addr:housenumber' in tag) and ('addr:postcode' in tag) and ('addr:city' in tag):
+					if (housenumber == tag['addr:housenumber']) and (street == tag['addr:street']) and (postcode == tag['addr:postcode'])\
+						 and (city == tag['addr:city']):
+
+						found[checked] = True
+						distance = math.sqrt((osm_object['lat'] - latitude) ** 2 + (osm_object['lon'] - longitude) ** 2)
+
+						# Modify object coordinates if it has been relocated. Keep the existing node if it has parents
+
+						if distance > 0.00001:
+
+							if osm_object['id'] in parents:
+								modify_object = osm_object.copy()
+								modify_object['tags'] = {}
+								osm_element (modify_object, action="modify")  # Keep empty node if parents
+								modified += 1
+
+								osm_object['lat'] = latitude
+								osm_object['lon'] = longitude
+								osm_element (osm_object, action="new")  # Create new addr node
+								added += 1
+
+							else:
+								osm_object['lat'] = latitude
+								osm_object['lon'] = longitude
+								osm_element (osm_object, action="modify")
+								modified += 1
+
+						del osm_data['elements'][found_index]  # Remove match to speed up looping
+						break
 
 	message ('\rChecking addresses... %i (%i corrected street names)\n' % (checked + 1, corrected))
 
@@ -275,12 +347,7 @@ if __name__ == '__main__':
 	# "Pure" address node are nodes which contain all of addr:street, addr:housenumber, addr:postcode, addr:city and no other tags
 	# Remaining non-matched addresses are output as new address nodes
 
-	filename = "Address_import_%s_%s.osm" % (municipality_id, municipality_name)
-	message ("\nGenerating file %s...\n" % filename)
-	file_out = open(filename, "w")
-
-	osm_line ("<?xml version='1.0' encoding='UTF-8'?>\n")
-	osm_line ("<osm version='0.6' generator='addr2osm v%s' upload='false'>\n" % version)
+	message ("\nCompleting file %s...\n" % filename)
 
 	i = -1
 	for row in addr_table2:
@@ -300,16 +367,13 @@ if __name__ == '__main__':
 
 				if street in corrections:
 					street = corrections[street]
-					street_corrected = True
-				else:
-					street_corrected = False
 
 				street = street.replace("'", u"’")
 				found_index = -1
 				modify = False
 
 				# Loop existing addr objects to find first close match with "pure" address node, to be modified
-				# Consider the match close if distance is less than 1/100.000 degrees
+				# Consider the match close if distance is less than 1/1000 degrees
 
 				for osm_object in osm_data['elements']:
 					found_index += 1
@@ -317,35 +381,50 @@ if __name__ == '__main__':
 
 						distance = math.sqrt((osm_object['lat'] - latitude) ** 2 + (osm_object['lon'] - longitude) ** 2)
 
-						if distance < 0.00001:
+						if distance < 0.001:
 							tag = osm_object['tags']
 							if ('addr:housenumber' in tag) and ('addr:postcode' in tag) and ('addr:city' in tag):
-								modify = True
-								modify_object = osm_object
+
+								keep_object = osm_object.copy()
 								del osm_data['elements'][found_index]
+								modify = True
 								break
 
 				# Output new addr node to file if no match, or modified addr node if close location match
  
 				if modify:
-					modified += 1
-					osm_line (u"  <%s id='%i' action='modify' timestamp='%s' uid='%i' user='%s' visible='true' version='%i' changeset='%i' lat='%f' lon='%f'>\n"\
-							% (modify_object['type'], modify_object['id'], modify_object['timestamp'], modify_object['uid'],\
-							modify_object['user'], modify_object['version'], modify_object['changeset'],\
-							modify_object['lat'], modify_object['lon']))
+					modify_object = keep_object.copy()
 				else:
-					osm_id -= 1
+					modify_object = {}
+					modify_object['type'] = "node"
+				
+				modify_object['tags'] = {
+					'addr:street': street,
+					'addr:housenumber': housenumber,
+					'addr:postcode': postcode,
+					'addr:city': city					
+				}
+
+				modify_object["lat"] = latitude
+				modify_object['lon'] = longitude
+
+				if modify:
+
+					if modify_object['id'] in parents:
+						keep_object['tags'] = {}
+						osm_element (keep_object, action="modify")  # Keeo empty node if parents
+						modified += 1
+
+						osm_element (modify_object, action="new")  # Create new addr node
+						added += 1
+
+					else:
+						osm_element (modify_object, action="modify")
+						modified += 1
+
+				else:
+					osm_element (modify_object, action="new")
 					added += 1
-					osm_line ("  <node id='%i' action='modify' visible='true' lat='%s' lon='%s'>\n" % (osm_id, row['Nord'], row['Øst']))
-
-				osm_tag ("addr:street", street)
-				osm_tag ("addr:housenumber", housenumber)
-				osm_tag ("addr:postcode", postcode)
-				osm_tag ("addr:city", city)
-				osm_line ("  </node>\n")
-
-				if street_corrected:
-					corrected_new += 1
 
 
 	# Output copy of remaining, non-matched addr objects to file (candidates for manual deletion of address tags and potentially also addr nodes)
@@ -357,9 +436,9 @@ if __name__ == '__main__':
 		if (osm_object['type'] == "node") and (len(osm_object['tags']) == 4) and\
 			('addr:housenumber' in osm_object['tags']) and ('addr:postcode' in osm_object['tags']) and ('addr:city' in osm_object['tags']):
 			deleted += 1
-			osm_element (osm_object, delete=True)
-		else:
-			osm_element (osm_object, delete=False)
+			osm_element (osm_object, action="delete")
+		elif debug:
+			osm_element (osm_object, action="output")
 
 	osm_line ("</osm>")
 
@@ -368,7 +447,7 @@ if __name__ == '__main__':
 	file_in.close()
 	file_out.close()
 
-	message ('  New addresses:                            %i (%i corrected street names)\n' % (added, corrected_new))
+	message ('  New addresses:                            %i\n' % added)
 	message ('  Updated existing address nodes:           %i\n' % modified)
 	message ('  Deleted existing address nodes:           %i\n' % deleted)
 	message ('  Remaining addresses in OSM without match: %i\n' % (len(osm_data['elements']) - deleted))
